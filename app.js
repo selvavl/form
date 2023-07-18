2339659e0011f4f6fa52f09259cbe0a861bfbb11
selvavl <vialopez.jm@gmail.com>
2023-07-18 11:40:36 -0500

    Form finished
    Form project finished

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const form = document.querySelector('form');
 const password = form.password;
 const confirmPassword = form.confirmPassword;
-const feedback = document.querySelectorAll('.feedback');
+const feedback = document.querySelector('.feedback');
 let isPasswordMatch = false;
 
 confirmPassword.addEventListener('input', () => {
     console.log("listening for password");
-    if (password.value!== confirmPassword.value) {
-        feedback.innerText = "Password did not match";
+    if (password.value != confirmPassword.value) {
+        if(!feedback.classList.contains("error")){
+            feedback.classList.toggle("error");
+            password.classList.toggle("error");
+            confirmPassword.classList.toggle("error");
+        }
         isPasswordMatch = false;
         console.log("false");
     } else {
+        if(feedback.classList.contains("error")) {
+            feedback.classList.toggle("error");
+            password.classList.toggle("error");
+            confirmPassword.classList.toggle("error");
+        }
         feedback.innerHTML = "";
         isPasswordMatch = true;
+        console.log("true");
     }
 });
